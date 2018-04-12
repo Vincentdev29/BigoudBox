b58daf0b2e32f7d60778d88c38646461c4717ab6
Vincentdev29 <vincent.cabillic@hotmail.fr>
2018-04-12 16:32:25 +0200

    Adding Settings & Shutdown Button to Forms (Form1.cs)

--- UserInterface_C#/BigoudBoxUserInterface/BigoudBoxUserInterface/Form1.Designer.cs
+++ UserInterface_C#/BigoudBoxUserInterface/BigoudBoxUserInterface/Form1.Designer.cs
@@ -30,11 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Settings_Button = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Quit_Button = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,16 +48,8 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(220, 720);
+            this.panel1.Size = new System.Drawing.Size(230, 720);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.Firebrick;
-            this.panel2.Location = new System.Drawing.Point(220, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1060, 60);
-            this.panel2.TabIndex = 1;
             // 
             // pictureBox1
             // 
@@ -65,6 +61,40 @@
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // Settings_Button
+            // 
+            this.Settings_Button.FlatAppearance.BorderSize = 0;
+            this.Settings_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Settings_Button.Image = ((System.Drawing.Image)(resources.GetObject("Settings_Button.Image")));
+            this.Settings_Button.Location = new System.Drawing.Point(883, -2);
+            this.Settings_Button.Name = "Settings_Button";
+            this.Settings_Button.Size = new System.Drawing.Size(75, 72);
+            this.Settings_Button.TabIndex = 1;
+            this.Settings_Button.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Firebrick;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.Settings_Button);
+            this.panel2.Controls.Add(this.Quit_Button);
+            this.panel2.Location = new System.Drawing.Point(230, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1050, 70);
+            this.panel2.TabIndex = 1;
+            // 
+            // Quit_Button
+            // 
+            this.Quit_Button.FlatAppearance.BorderSize = 0;
+            this.Quit_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Quit_Button.Image = ((System.Drawing.Image)(resources.GetObject("Quit_Button.Image")));
+            this.Quit_Button.Location = new System.Drawing.Point(975, -2);
+            this.Quit_Button.Name = "Quit_Button";
+            this.Quit_Button.Size = new System.Drawing.Size(75, 72);
+            this.Quit_Button.TabIndex = 0;
+            this.Quit_Button.UseVisualStyleBackColor = true;
+            this.Quit_Button.Click += new System.EventHandler(this.Quit_Button_Click);
+            // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
@@ -74,6 +104,14 @@
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.panel3.Location = new System.Drawing.Point(958, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(20, 70);
+            this.panel3.TabIndex = 3;
             // 
             // Form1
             // 
@@ -88,6 +126,7 @@
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
@@ -99,6 +138,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Settings_Button;
+        private System.Windows.Forms.Button Quit_Button;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
