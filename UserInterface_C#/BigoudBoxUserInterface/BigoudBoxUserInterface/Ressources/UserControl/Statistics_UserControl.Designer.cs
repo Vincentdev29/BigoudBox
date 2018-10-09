a5d7b6720cf34f390df96083b9bf7895ad433ceb
Aruma <Aruma_@DESKTOP-02T66VA>
2018-10-09 11:17:56 +0200

    Création Statistics User Control pour menu de statistiques

---
+++ UserInterface_C#/BigoudBoxUserInterface/BigoudBoxUserInterface/Ressources/UserControl/Statistics_UserControl.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace BigoudBoxUserInterface
+{
+    partial class Statistics_UserControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Meiryo UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(393, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "STATISTICS";
+            // 
+            // Statistics_UserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.label1);
+            this.Name = "Statistics_UserControl";
+            this.Size = new System.Drawing.Size(1050, 650);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
